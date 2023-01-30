d32aa54d558b163e0983311227594600cb0ea2ee
Roman-code-web <selenerg.275@gmail.com>
2023-01-30 09:21:53 -0500

    servicos producto

---
+++ src/app/modelo/producto.ts
@@ -0,0 +1,7 @@
+export interface Producto {
+    id?:string;
+    codPro:string;
+    descripcion:string;
+    cantidad:string;
+    precio:string;
+}
---
+++ src/app/servicios/productos.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { collectionData, Firestore } from '@angular/fire/firestore';
+import { addDoc, collection, deleteDoc, doc , updateDoc } from 'firebase/firestore';
+import { Observable } from 'rxjs';
+import { Producto } from '../modelo/producto';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+  //1.
+  constructor(private firestore : Firestore) { }
+
+  //funciones
+
+  agregarProducto(producto:Producto){
+    const productoRef = collection(this.firestore, 'productos');
+    return addDoc(productoRef, producto) 
+  }
+  obtenerProducto():Observable<Producto[]>{
+    const productoRef = collection(this.firestore, 'productos')
+    return collectionData(productoRef, {idField:'codPro'}) as Observable<Producto[]>
+  }
+  eliminarProducto( producto : Producto){
+    const productoRef = doc(this.firestore, `productos/${producto.id}`)
+    return deleteDoc(productoRef);
+  } 
+}
